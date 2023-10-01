9465e8804a2bdefebf445559a18faaaca3fe431c
pherdinandoe <144394226+pherdinandoe@users.noreply.github.com>
2023-10-01 18:52:04 -0400

    Updated Policy.java
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -1,5 +1,5 @@
 public class Policy {
-   //declare all private class fields
+   //dewclare all private class fields
    private String policyNum;
    private String providerName;
    private String firstName, lastName;
@@ -26,79 +26,147 @@ public class Policy {
       weight = iWeight;
    }
    
-   //setter methods for all fields
+   /**
+      The setPolicyNum method sets the policy number
+      @param a The policy number
+   */
    public void setPolicyNum(String a) {
       policyNum = a;
    }
    
+   /**
+      The setProviderName method sets the provider name
+      @param b The provider's name
+   */
    public void setProviderName(String b) {
       providerName = b;
    }
    
+   /**
+      The setFirstName method sets the policy holder's first name
+      @param c The policy holder's first name
+   */
    public void setFirstName(String c) {
       firstName = c;
    }
    
+   /**
+      The setLastName method sets the policy holder's last name
+      @param d The policy holder's last name
+   */
    public void setLastName(String d) {
       lastName = d;
    }
    
+   /**
+      The setSmokingStatus method sets the policy holder's smoking status
+      @param e The policy holder's smoking status
+   */
    public void setSmokingStatus(String e) {
       smokingStatus = e;
    }
    
+   /**
+      The setAge method sets the policy holder's age
+      @param f The policy holder's age
+   */
    public void setAge(int f) {
       age = f;
    }
    
+   /**
+      The setHeight method sets the policy number
+      @param a The policy number
+   */
    public void setHeight(int g) {
       height = g;
    }
    
+   /**
+      The setWeight method sets the policy holder's weight
+      @param h The policy holder's weight
+   */
    public void setWeight(int h) {
       weight = h;
    }
    
-   //getter methods for all class fields
+   /**
+      The getPolicyNum method gets the policy number
+      @return The policy number
+   */
    public String getPolicyNum() {
       return policyNum;
    }
    
+   /**
+      The getProviderName method gets the provider name
+      @return The provider's name
+   */
    public String getProviderName() {
       return providerName;
    }
    
+   /**
+      The getFirstName method gets the policy holder's first name
+      @return The policy holder's first name
+   */
    public String getFirstName() {
       return firstName;
    }
    
+   /**
+      The getLastName method gets the policy holder's last name
+      @return The policy holder's last name
+   */
    public String getLastName() {
       return lastName;
    }
    
+   /**
+      The getSmokingStatus method gets the policy holder's smoking status
+      @return The policy holder's smoking status
+   */
    public String getSmokingStatus() {
       return smokingStatus;
    }
    
+   /**
+      The getAge method gets the policy holder's age
+      @return The policy holder's age
+   */
    public double getAge() {
       return age;
    }
    
+   /**
+      The getHeight method gets the policy holder's height
+      @return The policy holder's height
+   */
    public double getHeight() {
       return height;
    }
    
+   /**
+      The getWeight method gets the policy holder's weight
+      @return The policy holder's weight
+   */
    public double getWeight() {
       return weight;
    }
    
-   //method to calculate BMI
+   /**
+      The calculateBMI method calculates the policy holder's BMI
+      @return The policy holder's BMI
+   */
    public double calculateBMI() {
       double BMI = (weight * 703 ) / (height * height);
       return BMI;
    }
    
-   //method to calculate policy price
+   /**
+      The calculatePrice method calculates the policy price
+      @return The policy price
+   */
    public double calculatePrice() {
       double price = 600;
       double BMI = calculateBMI();
