c1b584c565ad99c2d21d6aa0bf2deb5dd0336bd8
pherdinandoe <144394226+pherdinandoe@users.noreply.github.com>
2023-10-29 20:22:36 -0400

    Update Policy.java
    Moved fields and methods to the PolicyHolder

--- Policy.java
+++ Policy.java
@@ -2,28 +2,18 @@ public class Policy {
    //declare all private class fields
    private String policyNum;
    private String providerName;
-   private String firstName, lastName;
-   private String smokingStatus;
-   private int age;
-   private double height, weight;
-   
+   private PolicyHolder policyHolder;
+
    //no-arg constructor that sets default values for all fields
    public Policy() {
    }
    
    //constructor that takes arguments for all class fields
-   public Policy(String iPolicyNum, String iProviderName, String iFirstName, 
-                  String iLastName, String iSmokingStatus, int iAge, double iHeight, 
-                  double iWeight) {
+   public Policy(String policyNum, String providerName, PolicyHolder policyHolder) {
       //intialize all class fields
-      policyNum = iPolicyNum;
-      providerName = iProviderName;
-      firstName = iFirstName;
-      lastName = iLastName;
-      smokingStatus = iSmokingStatus;
-      age = iAge;
-      height = iHeight;
-      weight = iWeight;
+      this.policyNum = policyNum;
+      this.providerName = providerName;
+      this.policyHolder = new PolicyHolder(policyHolder); //assign copy of PolicyHolder object
    }
    
    /**
@@ -43,52 +33,14 @@ public class Policy {
    }
    
    /**
-      The setFirstName method sets the policy holder's first name
-      @param c The policy holder's first name
+      The setPolicyHolder method sets the provider name
+      @param policyHolder The policy holder object
    */
-   public void setFirstName(String c) {
-      firstName = c;
+   public void setPolicyHolder(PolicyHolder policyHolder) {
+      this.policyHolder = new PolicyHolder(policyHolder); //assign copy of object
    }
    
-   /**
-      The setLastName method sets the policy holder's last name
-      @param d The policy holder's last name
-   */
-   public void setLastName(String d) {
-      lastName = d;
-   }
-   
-   /**
-      The setSmokingStatus method sets the policy holder's smoking status
-      @param e The policy holder's smoking status
-   */
-   public void setSmokingStatus(String e) {
-      smokingStatus = e;
-   }
-   
-   /**
-      The setAge method sets the policy holder's age
-      @param f The policy holder's age
-   */
-   public void setAge(int f) {
-      age = f;
-   }
-   
-   /**
-      The setHeight method sets the policy number
-      @param a The policy number
-   */
-   public void setHeight(int g) {
-      height = g;
-   }
-   
-   /**
-      The setWeight method sets the policy holder's weight
-      @param h The policy holder's weight
-   */
-   public void setWeight(int h) {
-      weight = h;
-   }
+   //getter methods
    
    /**
       The getPolicyNum method gets the policy number
@@ -107,60 +59,11 @@ public class Policy {
    }
    
    /**
-      The getFirstName method gets the policy holder's first name
-      @return The policy holder's first name
+      The getPolicyHolder method returns a copy of the PolicyHolder object
+      @return A copy of the PolicyHolder object
    */
-   public String getFirstName() {
-      return firstName;
-   }
-   
-   /**
-      The getLastName method gets the policy holder's last name
-      @return The policy holder's last name
-   */
-   public String getLastName() {
-      return lastName;
-   }
-   
-   /**
-      The getSmokingStatus method gets the policy holder's smoking status
-      @return The policy holder's smoking status
-   */
-   public String getSmokingStatus() {
-      return smokingStatus;
-   }
-   
-   /**
-      The getAge method gets the policy holder's age
-      @return The policy holder's age
-   */
-   public double getAge() {
-      return age;
-   }
-   
-   /**
-      The getHeight method gets the policy holder's height
-      @return The policy holder's height
-   */
-   public double getHeight() {
-      return height;
-   }
-   
-   /**
-      The getWeight method gets the policy holder's weight
-      @return The policy holder's weight
-   */
-   public double getWeight() {
-      return weight;
-   }
-   
-   /**
-      The calculateBMI method calculates the policy holder's BMI
-      @return The policy holder's BMI
-   */
-   public double calculateBMI() {
-      double BMI = (weight * 703 ) / (height * height);
-      return BMI;
+   public PolicyHolder getPolicyHolder() {
+      return new PolicyHolder(policyHolder);
    }
    
    /**
@@ -169,16 +72,15 @@ public class Policy {
    */
    public double calculatePrice() {
       double price = 600;
-      double BMI = calculateBMI();
       
-      if (age > 50)
+      if (policyHolder.getAge() > 50)
          price += 75;
       
-      if (smokingStatus == "smoker")
+      if (policyHolder.getSmokingStatus() == "smoker")
          price += 100;
          
-      if (BMI > 35)
-         price += (BMI - 35) * 20;
+      if (policyHolder.calculateBMI() > 35)
+         price += (policyHolder.calculateBMI() - 35) * 20;
          
       return price;
    }
